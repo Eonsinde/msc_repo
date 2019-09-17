b2313ab97ffba15bdbbf052203314975706a23ab
olasinde <olasinde>
2019-09-17 01:58:02 +0100

    modified:   clinic/urls.py

--- clinic/urls.py
+++ clinic/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('msc/', include('msc.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
