ef00b57bcf19e2baa29545cba83e2e6fcc0c88b0
olasinde <olasinde>
2019-09-17 01:19:51 +0100

    Updated Settings

--- clinic/settings.py
+++ clinic/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG_VALUE', '') != 'False'
 
-SECRET_KEY = 'thisisawesome'
+SECRET_KEY = os.environ.get('MSC_SECRET_KEY')
 
 
 DEBUG = True
